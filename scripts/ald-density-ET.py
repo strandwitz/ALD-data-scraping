c2c0a090bdf675050696363775860ecf5ddc6a9c
Daria Zachariassen <dariaskye16@gmail.com>
2022-06-29 22:11:02 -0400

    added theoretical density google sheet

---
+++ scripts/ald-density-ET.py
@@ -0,0 +1,40 @@
+"""
+Plot Experimental vs Theoretical Densities
+
+"""
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from scipy import stats
+import matplotlib.pyplot as plt
+
+sheet_name_exp = "FilmProps"
+sheet_name_thr = "TheorDensity"
+sheet_id = "1CnYIYPMymwAKaVlElBk4ceNN2RtTxpKIHTzuTRjfY3s"
+url_exp = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name_exp}"
+url_thr = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name_thr}"
+
+# LOAD DATA
+df_exp = pd.read_csv(url_exp)
+df_thr = pd.read_csv(url_thr)
+
+# CLEAN DATA
+df_exp.dropna(axis='columns', how='all', inplace=True) # drop columns that are all NA values
+df_exp.columns = df_exp.columns.str.replace(r'[ ]{2,}',' ', regex=True) # make sure column names only have 1 space between words
+
+df_thr.dropna(axis='columns', how='all', inplace=True) # drop columns that are all NA values
+df_thr.columns = df_thr.columns.str.replace(r'[ ]{2,}',' ', regex=True) # make sure column names only have 1 space between words
+
+
+# VIEW DATA
+print(list(df_exp.columns), "\n") # view column names
+print(df_exp.info(), "\n") # view column names and types
+print(df_exp.head(), "\n") # view the first few rows of the dataframe
+
+print("-"*30)
+
+print(list(df_thr.columns), "\n") # view column names
+print(df_thr.info(), "\n") # view column names and types
+print(df_thr.head(), "\n") # view the first few rows of the dataframe
