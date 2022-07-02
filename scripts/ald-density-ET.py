a4ff3634a63a3fc941abda4006117e644b472e93
Daria Zachariassen <dariaskye16@gmail.com>
2022-07-01 20:05:02 -0400

    create df and plot

--- scripts/ald-density-ET.py
+++ scripts/ald-density-ET.py
@@ -17,9 +17,15 @@ sheet_id = "1CnYIYPMymwAKaVlElBk4ceNN2RtTxpKIHTzuTRjfY3s"
 url_exp = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name_exp}"
 url_thr = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name_thr}"
 
+url_exp = "data/data_exp.csv"
+url_thr = "data/data_thr.csv"
+
 # LOAD DATA
 df_exp = pd.read_csv(url_exp)
 df_thr = pd.read_csv(url_thr)
+
+# df_exp.to_csv("data/data_exp.csv", encoding='utf-8')
+# df_thr.to_csv("data/data_thr.csv", encoding='utf-8')
 
 # CLEAN DATA
 df_exp.dropna(axis='columns', how='all', inplace=True) # drop columns that are all NA values
@@ -32,10 +38,39 @@ df_thr.columns = df_thr.columns.str.replace(r'[ ]{2,}',' ', regex=True) # make s
 # VIEW DATA
 print(list(df_exp.columns), "\n") # view column names
 print(df_exp.info(), "\n") # view column names and types
-print(df_exp.head(), "\n") # view the first few rows of the dataframe
+# print(df_exp.head(), "\n") # view the first few rows of the dataframe
 
 print("-"*30)
 
 print(list(df_thr.columns), "\n") # view column names
 print(df_thr.info(), "\n") # view column names and types
-print(df_thr.head(), "\n") # view the first few rows of the dataframe
+# print(df_thr.head(), "\n") # view the first few rows of the dataframe
+
+
+
+# GET DATA TO PLOT
+
+exp_density = df_exp.loc[:,["Material", "Density (g.cm-3)"]]
+thr_density = df_thr.loc[:,["Material", "Density gcm3"]]
+
+exp_density.rename(columns={"Density (g.cm-3)": "Experimental Density"}, inplace=True)
+thr_density.rename(columns={"Density gcm3": "Theoretical Density"}, inplace=True)
+
+print(exp_density.head())
+print(thr_density.head())
+
+df_merged = pd.merge(exp_density, thr_density, on="Material", how="left")
+
+print(df_merged.tail(30))
+
+
+
+
+# PLOT
+
+
+df_merged.plot(x="Theoretical Density", y="Experimental Density", marker='x', kind='scatter') # scatter plot
+
+
+plt.show()
+
