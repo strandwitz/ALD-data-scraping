0abf0c1b4b7fc3b9c0b3f71527296616478ca01b
Daria Zachariassen <dariaskye16@gmail.com>
2022-07-07 16:48:40 -0400

    updated plot exp vs thr

--- scripts/ald-density-ET.py
+++ scripts/ald-density-ET.py
@@ -11,6 +11,7 @@ import seaborn as sns
 from scipy import stats
 import matplotlib.pyplot as plt
 import random
+from adjustText import adjust_text
 
 sheet_name_exp = "FilmProps"
 sheet_name_thr = "TheorDensity"
@@ -21,96 +22,142 @@ url_thr = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:cs
 # url_exp = "data/data_exp.csv"
 # url_thr = "data/data_thr.csv"
 
-# LOAD DATA
-df_exp = pd.read_csv(url_exp)
-df_thr = pd.read_csv(url_thr)
+def ald_print_info(df, label="", info=True, head=[True, 5], tail=[False, 5], cols=True, vc=[False, None]):
+    # VIEW DATA
+    print()
+    print(">"*60, f" {label}") # visual separator on terminal
+    if cols:
+        print(list(df.columns), "\n") # view column names
 
-# df_exp.to_csv("data/data_exp.csv", encoding='utf-8')
-# df_thr.to_csv("data/data_thr.csv", encoding='utf-8')
+    if info:
+        print(df.info(), "\n") # view column names and types
 
-# CLEAN DATA
-df_exp.dropna(axis='columns', how='all', inplace=True) # drop columns that are all NA values
-df_exp.columns = df_exp.columns.str.replace(r'[ ]{2,}',' ', regex=True) # make sure column names only have 1 space between words
+    if head[0]:
+        print(df.head(head[1]), "\n") # view the first few rows of the dataframe
 
-df_thr.dropna(axis='columns', how='all', inplace=True) # drop columns that are all NA values
-df_thr.columns = df_thr.columns.str.replace(r'[ ]{2,}',' ', regex=True) # make sure column names only have 1 space between words
+    if tail[0]:
+        print(df.tail(tail[1]), "\n") # view the last few rows of the dataframe
+
+    if vc[0]:
+        print(df[vc[1]].value_counts())
+
+    print("<"*60) # visual separator on terminal
 
 
-# VIEW DATA
-print(list(df_exp.columns), "\n") # view column names
-# # print(df_exp.info(), "\n") # view column names and types
-# # print(df_exp.head(), "\n") # view the first few rows of the dataframe
-# print(df_exp["Material"].value_counts())
-# print("-"*60) # visual separator on terminal
+def ald_load_data(urls, view_data=True):
 
-print(list(df_thr.columns), "\n") # view column names
-# # print(df_thr.info(), "\n") # view column names and types
-# # print(df_thr.head(), "\n") # view the first few rows of the dataframe
-# print(df_thr["Material"].value_counts())
-print("-"*60) # visual separator on terminal
+    df = 0
+    for url in urls:
+        df = pd.read_csv(url)
 
+        # CLEAN DATA
+        df.dropna(axis='columns', how='all', inplace=True) # drop columns that are all NA values
+        df.columns = df.columns.str.replace(r'[ ]{2,}',' ', regex=True) # make sure column names only have 1 space between words
+
+        if view_data:
+
+            # VIEW DATA
+            sheet = url.split("sheet=", 1)[1]
+            ald_print_info(df, label=sheet)
+
+        yield df
+
+
+data_generator = ald_load_data([url_exp, url_thr], view_data=False)
+df_exp = next(data_generator)
+df_thr = next(data_generator)
 
 # GET DATA TO PLOT
 
-exp_density = df_exp.loc[:,["Material", "Density (g.cm-3)"]]
-thr_density = df_thr.loc[:,["Material", "Density gcm3"]]
+def ald_merge_df(df1, df2, key, cols, view_data=True):
+    exp_density = df1.loc[:, [key, cols[0][0]]]
+    thr_density = df2.loc[:, [key, cols[1][0]]]
+    thr_phase = df2.loc[:, [key, *cols[1]]]
 
-NAME_EXP = "experimental"
-NAME_THR = "theoretical"
+    df_merged = pd.merge(exp_density, thr_density, on=key)
+    df_merged = pd.merge(df_merged, thr_phase, on=[key, cols[1][0]])
 
-exp_density.rename(columns={"Density (g.cm-3)": NAME_EXP}, inplace=True)
-thr_density.rename(columns={"Density gcm3": NAME_THR}, inplace=True)
-
-# print(exp_density.head())
-# print(thr_density.head())
-# print("-"*60) # visual separator on terminal
-
-df_merged = pd.merge(exp_density, thr_density, on="Material")
-
-# print(df_merged["Material"].value_counts())
-# print(df_merged.info())
-# print(df_merged.head())
-# print("-"*60) # visual separator on terminal
-
-def get_tidy_df(df1, df2, cat, val, idv):
-
-    df_m1 = pd.melt(df1, id_vars=idv, var_name=cat, value_name=val)
-    df_m1.dropna(inplace=True)
-    # print(df_m1.info())
-
-    df_m2 = pd.melt(df2, id_vars=idv, var_name=cat, value_name=val)
-    df_m2.dropna(inplace=True)
-    # print(df_m2.info())
-
-    df_merged = pd.concat([df_m1, df_m2], ignore_index=True)
-    # print(df_merged.info())
+    if view_data:
+        ald_print_info(df_merged, label=" MERGED")
 
     return df_merged
 
-# df_tidy = get_tidy_df(exp_density, thr_density, cat="Type", val="Density", idv="Material")
-# print(df_tidy.info())
-# print(df_tidy.head())
 
-def tidy_handler(df, group, y):
-    # gb = df.groupby(group)
-    # df_mean = gb[y].agg(np.mean)
-    # df_mean = df_mean.reset_index()
+# PRINT COLUMN NAMES
+exp_all_columns = list(df_exp.columns)
+print("EXPERIMENTAL ",exp_all_columns)
 
-    # df['type_bool'] = (df['Type'] == "experimental").map({False: "theoretical", True: "experimental"})
 
-    # df.loc[]
-    print(df.head(20), "\n")
-    print(df.info(), "\n")
+thr_all_columns = list(df_thr.columns)
+print("THEORETICAL ",thr_all_columns)
 
-# tidy_handler(df_tidy, ["Type", "Material"], y="Density")
-# print(df_tidy.info())
-# print(df_tidy.head())
+
+# RENAME DENSITY COLUMNS
+density_exp = "Experimental Density (g.cm-3)"
+density_thr = "Theoretical Density (g.cm-3)"
+
+re_cols = {
+    "exp": {
+        "Density (g.cm-3)": density_exp,
+        "Tdep (°C)": "Deposition Temperature (°C)"
+    }, 
+    "thr": {"Density gcm3": density_thr}
+}
+
+df_exp.rename(columns=re_cols['exp'], inplace=True)
+df_thr.rename(columns=re_cols['thr'], inplace=True)
+
+
+# CHOOSE KEY
+key = "Material"
+
+# SELECT COLUMNS
+cols_exp = ['Source DOI', key, density_exp, 
+            'Deposition Temperature (°C)', 'Tpostdep', 'PEALD?', 'Density Meas',
+            'k', 'thickness of meas. k (nm)', 'RF Bias (V)', 
+            # 'P1 (metal)', 'P2 (nonmetal)', 'P3 (optional)', 'P4',
+            'Paper also measured']
+df_exp = df_exp[cols_exp]
+
+cols_thr = [key, 'Phase', density_thr, 'Space group']
+df_thr = df_thr[cols_thr]
+
+
+# MERGE DATAFRAMES
+cols = ([density_exp], [density_thr, "Phase"])
+df_merged = ald_merge_df(df_exp, df_thr, key, cols, view_data=False)
+
+
+
+# SORT VALUES
+def sort_valuecounts(df, key):
+    df = df.loc[:] # prevent SettingWithCopyWarning
+    df['freq'] = df.groupby(key)[key].transform('count')
+    df.sort_values(['freq',key], ascending=False, inplace=True)
+    df.drop('freq', axis=1, inplace=True)
+
+    df.reset_index(drop=True, inplace=True)
+
+    return df
+
+
+df_exp = sort_valuecounts(df_exp, key)
+df_thr = sort_valuecounts(df_thr, key)
+df_merged = sort_valuecounts(df_merged, key)
+
+
+# PRINT DF INFO
+ald_print_info(df_exp, label="DF EXPERIMENTAL", vc=[False, key])
+ald_print_info(df_thr, label="DF THEORETICAL", vc=[False, key])
+ald_print_info(df_merged, label="DF MERGED", vc=[False, key], head=[False, 0], tail=[True, 10])
+
 
 
 
 # PLOT
 
 def plot_data(df, x, y, z):
+    print("PLOT", "-"*60)
 
     fig, ax = plt.subplots(figsize=(16,9)) # figsize=(6, 7)
     # ax.grid(True, color = '#e8e8e6', linestyle = '--', linewidth = 0.5)
@@ -132,8 +179,8 @@ def plot_data(df, x, y, z):
     df2 = df.loc[:,[x,y,z]]
     df2.sort_values(by=[x,y], ascending=True, inplace=True)
     df2.drop_duplicates(subset=cols, keep='last', inplace=True)
-    print(df2.head())
-    print(df2.shape)
+    # print(df2.head())
+    # print(df2.shape)
     for line in range(0,df2.shape[0]):
         ox=0
         oy=0.1
@@ -203,16 +250,104 @@ def plot_swarm(df, x, y, z):
 
     return fig
 
+def plot_data1(df, x, y, z, point_labels,  **kwargs):
+    ald_print_info(df, label="DF PLOTTING")
+
+    fig, ax = plt.subplots(figsize=(16,9)) # figsize=(6, 7)
+
+    order_z = list(df[z].unique())
+
+    ax.axline((0,0), slope=1, color='silver', lw=1, ls='--', alpha=0.5, label='_none_')
+    g1 = sns.scatterplot(ax=ax, data=df, x=x, y=y, hue=z, alpha=0.6, s=35, style=z, hue_order=order_z, style_order=order_z)
+    g2 = sns.scatterplot(ax=ax, data=df, x=x, y=x, color="black", alpha=0.9, s=15, marker="+")
+
+    df_lbls = df.loc[:, [x, y, *point_labels]]
+    df_lbls.drop_duplicates(subset=[x, z], keep='first', inplace=True)
 
 
-# # df_merged.plot(x=NAME_THR, y=NAME_EXP, marker='x', kind='scatter') # scatter plot
-# fig1 = plot_data(df_merged, x=NAME_THR, y=NAME_EXP, z="Material")
-fig2 = plot_data(df_merged, x=NAME_THR, y=NAME_EXP, z="Material")
+
+    print(df_lbls.head())
+    print(df_lbls.shape)
+
+    texts = []
+
+    for line in range(0,df_lbls.shape[0]):
+        lbls = list(df_lbls.loc[:,point_labels].iloc[line])
+
+        ox=1.0
+        oy=1.0
+
+
+        pnt_lbl = ""
+        if (len(lbls) > 1) and (lbls[0] in lbls[1]):
+            pnt_lbl = lbls[1]
+            ox=0.5
+            oy=0.5
+            
+        else:
+            lbls = [lbls[0], *lbls[1].split(" ", 1)]
+            if len(lbls) > 2:
+                ox = 3.25
+                oy = 3.25
+            elif len(lbls) == 2:
+                ox = 2.0
+                oy = 2.0
+
+            pnt_lbl = "\n".join(lbls)
+
+        if ox == 2.0:
+            if line % 2 == 0:
+                ox *= 0.5
+                oy *= 0.5
+            elif line % 2 == 1:
+                ox *= 1
+                oy *= 1
+     
+        
+        px=df_lbls[x].iloc[line]
+        py=df_lbls[x].iloc[line]
+        ty=df_lbls[y].iloc[line]
+        if py > ty: 
+            ty = py
+
+        ylim = (0.98) * ax.get_ylim()[1]
+        xlim = (0.98) * ax.get_xlim()[1]
+        
+        while ty+oy > ylim:
+            oy *= 0.99
+
+        while px-ox > xlim:
+            ox *= 0.99
+
+
+
+        # print(f"<{pnt_lbl}>\tx:{px} y:{py}")
+
+        texts.append(plt.annotate(pnt_lbl, 
+                    xy=(px, py), xytext=(px-ox, ty+oy), 
+                    verticalalignment='top', # horizontalalignment='center', 
+                    size=7, color='black', weight='light',
+                    arrowprops={"arrowstyle":"-|>, widthA=.4, widthB=.4",
+                                "connectionstyle":"arc3,rad=-.1",
+                                "color":"gray", "alpha":0.5
+                    })
+        )
+
+    # adjust_text(texts)
+
+    return fig
+
+
+
+
+# # df_merged.plot(x=density_thr, y=density_exp, marker='x', kind='scatter') # scatter plot
+# fig1 = plot_data(df_merged, x=density_thr, y=density_exp, z="Material")
+fig2 = plot_data1(df_merged, x=density_thr, y=density_exp, z=key, point_labels=[key, "Phase"])
 # fig2 = plot_swarm(df_tidy, x="Type", y="Density", z="Material")
 
 
 # fig1.savefig('plots/plotDensities.png', dpi=200, bbox_inches="tight")
-fig2.savefig('plots/plotDensities-2.png', dpi=200, bbox_inches="tight")
+fig2.savefig('plots/plotDensities-3.png', dpi=200, bbox_inches="tight")
 
 # plt.show()
 
