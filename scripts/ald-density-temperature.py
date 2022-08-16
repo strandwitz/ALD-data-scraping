001e10116ed16df38e51312942cd916713272589
Daria Zachariassen <dariaskye16@gmail.com>
2022-08-16 16:38:03 -0400

    ald solo plot styles

--- scripts/ald-density-temperature.py
+++ scripts/ald-density-temperature.py
@@ -46,6 +46,7 @@ cols = {"Tdep (°C)": "Deposition Temperature (°C)",
 x_tdep="Deposition Temperature (°C)"
 y_density="Density $(g.cm^{-3})$"
 z_material="Material"
+col_peald = "PEALD?"
 
 INPUT_MATERIAL = "Al2O3"
 
@@ -60,22 +61,44 @@ df[z_material] = df[z_material].str.strip() # Remove leading and trailing whites
 
 filt_material = df[z_material].str.contains(INPUT_MATERIAL)
 df_material = df.loc[filt_material]
+# df_material[col_peald] = df_material[col_peald].map({True: "PEALD", False: "Thermal"})
 print(df_material.info())
 
 
 # PLOT
+# plt.rc('axes', linewidth=1.5, edgecolor="red")
 
-def plot_data(df, x, y, hue, **kwargs):
-    cl="darkslateblue"
 
-    fig = sns.lmplot(data=df, x=x, y=y, 
-        hue=hue, hue_order=[True, False], markers=["^", "v"], palette=["orangered", "navy"],
-        ci=None, fit_reg=False,
-        scatter_kws={"s": 45, "alpha":0.5}, 
-        line_kws={"lw":1.5, "alpha":0.5})
+
+def plot_data(df, x, y, hue, info={}):    
+    hue_order = info.get("hue_order", None)
+    palette = info.get("palette", ["orangered", "navy"])
+    markers = info.get("markers", ["d", "s"])
+
+    scatter_kws = {"s": 45, "alpha":0.65}
+    line_kws = {"lw": 1.5, "alpha":0.70, "color": "darkslateblue"}
+
+    scatter_kws.update(info.get("scatter_kws", {}))
+    line_kws.update(info.get("line_kws", {}))
+
+    fig = sns.lmplot(data=df, x=x, y=y,
+        hue=hue, hue_order=hue_order, markers=markers, palette=palette,
+        ci=None, fit_reg=False, scatter_kws=scatter_kws)
+
+    sns.despine(top=False, right=False, left=False, bottom=False)
 
     sns.regplot(data=df, x=x, y=y, scatter=False, ci=None, 
-        line_kws={"lw":1.5, "alpha":0.7, "color":cl}).set(title=daz.create_latex_labels(kwargs.get("title")))
+        line_kws=line_kws).set(title=daz.create_latex_labels(info.get("title", "")))
+
+
+    # LEGEND
+    fig._legend.set_title(info.get("legend_title", ''))
+    if new_labels := info.get('legend_labels', False):
+        for t, l in zip(fig._legend.texts, new_labels):
+            t.set_text(l)
+
+    sns.move_legend(fig, "upper left", bbox_to_anchor=(0.83, 0.99))
+
 
     return fig
 
@@ -87,19 +110,36 @@ def plot_data2(df, x, y, hue, **kwargs):
         data=df,
         hue=hue, hue_order=[True, False],
         height=4, aspect=1.25,
-        hue_kws={"edgecolor": [c1,c2], 'facecolor':["none", c2], "markers":["^", "v"]},
+        hue_kws={"edgecolor": [c1,c2], 'facecolor':["none", c2], "markers":["^", "s"]},
     )
+
+    sns.despine(top=False, right=False, left=False, bottom=False)
+
     g.map(sns.scatterplot, x, y, linewidth=1.5, alpha=1)
     g.add_legend()
 
 
     sns.regplot(data=df, x=x, y=y, scatter=False, ci=None, 
         line_kws={"lw":1.5, "alpha":0.9, "color":c2}).set(title=daz.create_latex_labels(kwargs.get("title")))
+    
+    # print(sns.axes_style())
+
+    # LEGEND
+    g._legend.set_title(info.get("legend_title", ''))
+    if new_labels := info.get('legend_labels', False):
+        for t, l in zip(g._legend.texts, new_labels):
+            t.set_text(l)
+
+    sns.move_legend(g, "upper left", bbox_to_anchor=(0.83, 0.99))
 
 
     return g
 
-fig = plot_data(df_material, x=x_tdep, y=y_density, hue="PEALD?", title=INPUT_MATERIAL)
+info = dict(title=INPUT_MATERIAL, 
+    legend_labels=['PEALD', 'Thermal'], hue_order = [True, False], 
+    palette = ["orangered", "navy"])
+
+fig = plot_data(df_material, x=x_tdep, y=y_density, hue="PEALD?", info=info)
 fig.savefig('plots/plot4.png', dpi=300, bbox_inches="tight")
 
 
